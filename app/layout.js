f171a38017b9a2bd43585eb33d14bbd8f1472379
SantiParris8 <santiiparris@gmail.com>
2024-11-13 19:32:34 -0300

    layout.js

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import './globals.css'
+
+export const metadata = {
+  title: 'NearForce - Salesforce Excellence',
+  description: 'Leading Salesforce consulting firm delivering exceptional CRM solutions.',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+}

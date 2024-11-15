c4f608b6bc544ba257f22148841c6b34d74e20c5
SantiParris8 <santiiparris@gmail.com>
2024-11-15 16:46:01 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -171,7 +171,7 @@ export default function Home() {
       </section>
 
       {/* Services Section */}
-      <section id="services" className="py-20">
+      <section id="services" className="py-20 bg-white-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center mb-16">
             <h2 className="text-3xl font-bold">Our Services</h2>
@@ -267,7 +267,7 @@ export default function Home() {
       </div>
 
       {/* Contact Form */}
-      <section id="contact" className="py-20">
+      <section id="contact" className="py-20  bg-white-10">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex flex-col md:flex-row items-center gap-12">
             <div className="flex-1 relative h-[600px]">

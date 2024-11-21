3f8e647631cab3b02472fd5e6dffdfd1aee67359
SantiParris8 <santiiparris@gmail.com>
2024-11-21 15:30:05 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -156,7 +156,7 @@ export default function Home() {
           </div>
         </div>
       </section>
-
+ 
       {/* Stats Section */}
       <section className="py-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
